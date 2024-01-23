398c37f81e4d0f3e3c1a5449a6628776db7355c8
Sema Topcu <ssemaatopcu.@gmail.com>
2024-01-23 20:56:49 +0300

    Initial Commit

---
+++ listofimages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listofimages
+//
+//  Created by Sema Topcu on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
